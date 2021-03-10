a033c6401fe5521961dfe550155ec9b8b10ab6a5
Laken565 <80352697+Laken565@users.noreply.github.com>
2021-03-10 12:25:34 +0200

    Add project files.

---
+++ CRUDDemo/Controllers/OrderItemsController.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CRUDDemo.Models;
+
+namespace CRUDDemo.Controllers
+{
+    public class OrderItemsController : Controller
+    {
+        private CRUDDemoEntities11 db = new CRUDDemoEntities11();
+
+        // GET: OrderItems
+        public async Task<ActionResult> Index()
+        {
+            var orderItems = db.OrderItems.Include(o => o.Order).Include(o => o.Product);
+            return View(await orderItems.ToListAsync());
+        }
+
+        // GET: OrderItems/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            OrderItem orderItem = await db.OrderItems.FindAsync(id);
+            if (orderItem == null)
+            {
+                return HttpNotFound();
+            }
+            return View(orderItem);
+        }
+
+        // GET: OrderItems/Create
+        public ActionResult Create(int? id)
+        {
+           if (id == null)
+            {
+                ViewBag.OrderId = new SelectList(db.Orders, "Id", "OrderNumber");
+                ViewBag.ProductId = new SelectList(db.Products, "Id", "Name");
+            }
+            else
+            {
+                ViewBag.OrderId = new SelectList(db.Orders.Where(p=> p.Id==id), "Id", "OrderNumber");
+                ViewBag.ProductId = new SelectList(db.Products, "Id", "Name");
+            }
+       
+            return View();
+        }
+
+
+
+        // POST: OrderItems/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,OrderId,Name,Quantity,ProductId")] OrderItem orderItem)
+        {
+            if (ModelState.IsValid)
+            {
+                db.OrderItems.Add(orderItem);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.OrderId = new SelectList(db.Orders, "Id", "OrderNumber", orderItem.OrderId);
+            ViewBag.ProductId = new SelectList(db.Products, "Id", "Name", orderItem.ProductId);
+            return View(orderItem);
+        }
+
+        // GET: OrderItems/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            OrderItem orderItem = await db.OrderItems.FindAsync(id);
+            if (orderItem == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.OrderId = new SelectList(db.Orders, "Id", "OrderNumber", orderItem.OrderId);
+            ViewBag.ProductId = new SelectList(db.Products, "Id", "Name", orderItem.ProductId);
+            return View(orderItem);
+        }
+
+        // POST: OrderItems/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,OrderId,Name,Quantity,ProductId")] OrderItem orderItem)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(orderItem).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewBag.OrderId = new SelectList(db.Orders, "Id", "OrderNumber", orderItem.OrderId);
+            ViewBag.ProductId = new SelectList(db.Products, "Id", "Name", orderItem.ProductId);
+            return View(orderItem);
+        }
+
+        // GET: OrderItems/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            OrderItem orderItem = await db.OrderItems.FindAsync(id);
+            if (orderItem == null)
+            {
+                return HttpNotFound();
+            }
+            return View(orderItem);
+        }
+
+        // POST: OrderItems/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            OrderItem orderItem = await db.OrderItems.FindAsync(id);
+            db.OrderItems.Remove(orderItem);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ CRUDDemo/Controllers/OrdersController.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CRUDDemo.Models;
+
+namespace CRUDDemo.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private CRUDDemoEntities11 db = new CRUDDemoEntities11();
+
+        // GET: Orders
+        public async Task<ActionResult> Index()
+        {
+            var orders = db.Orders.Include(o => o.Customer);
+            return View(await orders.ToListAsync());
+        }
+
+        // GET: Orders/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Order order = await db.Orders.FindAsync(id);
+            if (order == null)
+            {
+                return HttpNotFound();
+            }
+            return View(order);
+        }
+
+        // GET: Orders/Create
+        public ActionResult Create(int? id)
+        {
+            if (id == null)
+            {
+                ViewBag.CustomerID = new SelectList(db.Customers, "id", "name");
+            }
+            else
+            {
+                ViewBag.CustomerID = new SelectList(db.Customers.Where(p => p.id == id), "id", "name");
+                // ViewBag. = new SelectList(db.rooms.Where(p => p.hotel_id == 626191), "room_id", "title");
+
+            }
+            return View();
+        }
+
+        // POST: Orders/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,OrderNumber,Description,Date,CustomerID")] Order order)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Orders.Add(order);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CustomerID = new SelectList(db.Customers, "id", "name", order.CustomerID);
+            return View(order);
+        }
+
+        // GET: Orders/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Order order = await db.Orders.FindAsync(id);
+            if (order == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CustomerID = new SelectList(db.Customers, "id", "name", order.CustomerID);
+            return View(order);
+        }
+
+        // POST: Orders/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,OrderNumber,Description,Date,CustomerID")] Order order)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(order).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CustomerID = new SelectList(db.Customers, "id", "name", order.CustomerID);
+            return View(order);
+        }
+
+        // GET: Orders/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Order order = await db.Orders.FindAsync(id);
+            if (order == null)
+            {
+                return HttpNotFound();
+            }
+            return View(order);
+        }
+
+        // POST: Orders/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Order order = await db.Orders.FindAsync(id);
+            db.Orders.Remove(order);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
